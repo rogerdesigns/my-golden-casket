b5141c855a6762fef3e0e3ce5fbb9c0619d7a428
Roger Wang <rogerdesigns@gmail.com>
2019-06-13 13:57:33 +1000

    extract routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,24 +3,22 @@ import { Routes, RouterModule } from '@angular/router';
 import { PowerballComponent } from './powerball/powerball.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'powerball',
+  component: PowerballComponent
+},
+{
+  path: '',
+  redirectTo: 'powerball',
+  pathMatch: 'full'
+},
+{
+  path: '**',
+  component: PageNotFoundComponent
+}];
 
 @NgModule({
-  imports: [RouterModule.forRoot([
-    {
-      path: 'powerball',
-      component: PowerballComponent
-    },
-    {
-      path:'',
-      redirectTo:'powerball',
-      pathMatch:'full'
-    },
-    {
-      path: '**',
-      component: PageNotFoundComponent
-    }
-  ])],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
